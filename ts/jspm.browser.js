088a7c995795afb62bfe169384bec767e6844bd6
Kallyn Gowdy <kallyngowdy@gmail.com>
2016-03-11 21:26:39 -0500

    Add Ignored Files

---
+++ ts/jspm.browser.js
@@ -0,0 +1,8 @@
+SystemJS.config({
+    paths: {
+        "github:*": "jspm_packages/github/*",
+        "npm:*": "jspm_packages/npm/*",
+        "main-bundle/": "/",
+        "ng2-firebase-demo-ts/": "app/"
+    }
+});
