1659738ecace3d195986c97de448fada6114c4ce
Kallyn Gowdy <kallyngowdy@gmail.com>
2016-03-11 22:19:37 -0500

    Update JavaScript Demo to Use FirebaseArray

--- js/app/firebase-hero.service.js
+++ js/app/firebase-hero.service.js
@@ -1,27 +1,25 @@
-(function (app) {
+(function(app) {
     app.FirebaseHeroService = ng.core
         .Class({
-            constructor: function (firebaseService) {
-                this.service = firebaseService.child('heroes');
+            constructor: function(firebaseService) {
+                this.array = firebaseService.child('heroes').asArray();
             },
 
-            getHeroes: function () {
+            getHeroes: function() {
                 var _this = this;
-                var service = _this.service;
-                return service.value.map(function (heroes) {
-                    return heroes.map(function (h, i) {
-                        // TODO: Cleanup
-                        return {
-                            id: h.id,
-                            name: h.name,
-                            save: function () {
-                                return service.child(i.toString()).setData({
-                                    id: this.id,
-                                    name: this.name
-                                });
-                            }
+                var arr = _this.array;
+                return arr.map(function(h, i) {
+                    // TODO: Cleanup
+                    return {
+                        id: h.id,
+                        name: h.name,
+                        save: function() {
+                            return arr.set(i, {
+                                id: this.id,
+                                name: this.name
+                            });
                         }
-                    })
+                    };
                 });
             }
         });
